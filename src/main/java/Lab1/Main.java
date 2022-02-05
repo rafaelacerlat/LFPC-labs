9ed521b16a8c2f35786bff3feca0c7aef725c876
Rafaela <rafaela.cerlat@gmail.com>
2022-02-05 22:37:35 +0200

    Regular grammar to Finite Automata (changes)

--- src/main/java/Lab1/Main.java
+++ src/main/java/Lab1/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Main {
 
-    private static final String inputPath = "C:\\Users\\rafae\\Desktop\\FAF\\FAF-sem4\\LFPC\\lfpc-labs\\src\\main\\java\\Lab1\\grammar.txt";
+    private static final String inputPath = "src/main/java/Lab1/grammar.txt";
 
     public static void main(String[] args) throws IOException {
         Grammar grammar = Grammar.parseFile(inputPath);
